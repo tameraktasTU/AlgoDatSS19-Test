d54ff4b0d02fdd7da637aabc06e70954ae0318ec
Afferore43 <45694132+Afferore43@users.noreply.github.com>
2019-05-27 23:15:15 +0200

    korrektur test genomics.java
    Fix falscher Wert bei Zeile 21 und 21

--- Blatt05/tests/GenomicsTest.java
+++ Blatt05/tests/GenomicsTest.java
@@ -8,17 +8,17 @@ class GenomicsTest {
     void optBottomUp() {
         String[] dict = {"AGT", "CA", "CAG", "GTC", "TC", "TCA", "TCC"};
         String[] dict2 = {"ab", "aa", "bb", "bbb", "aba", "bbba", "aaaabb", "a", "aaababbbaaababbbabaaa", "aababbbabaaaaabbbababbb", "aaababbbaaababbbabaaaaabbbababbb", "b"};
-        String[] dict3 = {"x", "xx", "xd"};
+        String[] dict3 = {"x", "xx", "xd", "xdx", "xxx", "xxd"};
         long k = Genomics.optBottomUp("CAGTCCAGTCAGTC", dict);
         long k2 = Genomics.optBottomUp("CAGTCCAGBTCAGTC", dict);
         long k3 = Genomics.optBottomUp("CAGTCCAGBTCAGTCB", dict);
         long k4 = Genomics.optBottomUp("aaababbbaaababbbabaaaaabbbababbb", dict2);
-        long k5 = Genomics.optBottomUp("xxxxxdxxxdxxxxxxdxxxdxxxxxxxxdxxxxxxxxdxxxxxxxxxdxxxxxxxxxx", dict3);
+        long k5 = Genomics.optBottomUp("xxxxxdxxxdxxxxxxdxxxdxxxxxxxxdxxxxxxxdxxd", dict3);
 
         assertEquals(4, k);
         assertEquals(0, k2);
         assertEquals(0, k3);
-        assertEquals(25541, k4);
-        assertEquals(16800, k5);
+        assertEquals(3307134, k4);
+        assertEquals(64943516, k5);
     }
 }
